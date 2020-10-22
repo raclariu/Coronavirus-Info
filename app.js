66e6a17c0301796953fbedf6eaaef4e6f214925b
CLAUDIU RACLARIU <ssrakla@gmail.com>
2020-10-22 19:01:33 +0300

    working chart under table row

--- app.js
+++ app.js
@@ -1,44 +1,3 @@
-// async function getData() {
-// 	let arr = [];
-// 	const res = await fetch('https://api.covid19api.com/dayone/country/romania/status/confirmed');
-// 	const data = await res.json();
-// 	console.log(data);
-// 	data.forEach(element => {
-// 		arr.push({ x: element.Date.replace('T00:00:00Z', ''), y: element.Cases });
-// 	});
-// 	let options = {
-// 		chart  : {
-// 			height     : 380,
-// 			width      : '100%',
-// 			type       : 'bar',
-// 			animations : {
-// 				initialAnimation : {
-// 					enabled : false
-// 				}
-// 			}
-// 		},
-// 		series : [
-// 			{
-// 				name : 'Cases',
-// 				data : arr
-// 			}
-// 		],
-// 		xaxis  : {
-// 			type   : 'datetime',
-// 			labels : {
-// 				datetimeFormatter : {
-// 					year  : 'yyyy',
-// 					month : "MMM 'yy"
-// 				}
-// 			}
-// 		},
-// 		colors : [ '#9C27B0' ]
-// 	};
-
-// 	let chart = new ApexCharts(document.querySelector('#chart'), options);
-// 	chart.render();
-// }
-
 let globalData;
 const allTableHeaders = document.querySelectorAll('th');
 
@@ -63,6 +22,10 @@ async function getSummary() {
 }
 
 async function renderMainTable(data) {
+	const chart = document.querySelector('.chart');
+	if (chart !== null) {
+		chart.remove();
+	}
 	const removeTableLeaveHeaders = document.querySelectorAll('.main-table-body tr:not(:first-child)');
 	removeTableLeaveHeaders.forEach(tr => tr.remove());
 	const mainTableBody = document.querySelector('.main-table-body');
@@ -113,6 +76,7 @@ function sortMainTableCols(clickedOn, e) {
 		}
 	});
 	renderMainTable(sorted);
+	test();
 }
 
 // Event listeners
@@ -125,15 +89,73 @@ allTableHeaders.forEach(header => {
 	});
 });
 
+async function callThings() {
+	await getSummary();
+	test();
+}
+
+window.addEventListener('load', callThings);
 // add chart below clicked table row country
+let country = '';
 function test() {
-	const trNoHeader = document.querySelectorAll('.main-table-body tr:not(:first-child)');
+	const trNoHeader = document.querySelectorAll('.main-table-body tr:not(:first-child) td:first-child');
 	trNoHeader.forEach(el => {
 		el.addEventListener('click', e => {
-			console.log(e.target);
+			const el = document.querySelector('.chart');
+			console.dir(el);
 			console.dir(e.target);
+			if (el !== null) {
+				el.remove();
+			}
+
+			country = e.target.innerText.toLowerCase().replace('(', '').replace(')', '');
+			const clickedParent = e.target.parentElement;
+			const newDiv = document.createElement('div');
+			newDiv.classList.add('chart');
+			clickedParent.insertAdjacentElement('afterend', newDiv);
+			console.log(country);
+			if (country) {
+				getData(country);
+			}
 		});
 	});
 }
 
-getSummary();
+async function getData(country) {
+	console.log(country);
+	let arr = [];
+	const res = await fetch(
+		`https://api.covid19api.com/total/country/${country}/status/confirmed?from=2020-10-01T00:00:00Z&to=2020-10-21T00:00:00Z`
+	);
+	const data = await res.json();
+	console.log(data);
+	data.forEach(element => {
+		arr.push({ x: element.Date.replace('T00:00:00Z', ''), y: element.Cases });
+	});
+	let options = {
+		chart  : {
+			height : 400,
+			width  : 1150,
+			type   : 'area'
+		},
+		series : [
+			{
+				name : 'Cases',
+				data : arr
+			}
+		],
+		xaxis  : {
+			type   : 'datetime',
+			labels : {
+				datetimeFormatter : {
+					year  : 'yyyy',
+					month : "MMM 'yy"
+				}
+			}
+		},
+		colors : [ 'rgb(78, 125, 228)' ]
+	};
+
+	let chart = new ApexCharts(document.querySelector('.chart'), options);
+	chart.render();
+}
