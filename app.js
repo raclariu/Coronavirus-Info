6c65ad8cd4262fbc28051e34a60659b31ee25cd7
CLAUDIU RACLARIU <ssrakla@gmail.com>
2020-10-27 19:15:56 +0200

    simple main table with charts

--- app.js
+++ app.js
@@ -54,18 +54,25 @@ async function renderMainTable(data) {
 	});
 }
 
+// Function that sorts column tables when a table header is clicked
 function sortMainTableCols(clickedOn, e) {
+	// Copy the countries data array as we don't want to mutate the original
 	const data = [ ...globalData.Countries ];
+
+	// Add a new key/value to each object in data array that represents the active coronavirus cases
 	data.forEach(obj => {
 		obj.ActiveCases = obj.TotalConfirmed - obj.TotalDeaths - obj.TotalRecovered;
 	});
 
+	// Only the clicked header should have the 'sorted' class
 	allTableHeaders.forEach(header => {
+		console.log(header);
 		if (header !== e.target) {
 			header.classList.remove('sorted');
 		}
 	});
 
+	// Sorting function
 	let sorted = data.sort((a, b) => {
 		let first = a[clickedOn];
 		let second = b[clickedOn];
@@ -77,102 +84,158 @@ function sortMainTableCols(clickedOn, e) {
 			if (second > first) return -1;
 		}
 	});
-	console.log(sorted);
+
+	// Re-render the main table with the new sorted data
 	renderMainTable(sorted);
-	test();
+
+	// Because we re-render the table, we need to re-do the event listener for each country cell
+	createChartContainer();
 }
 
 // Event listeners
-
 allTableHeaders.forEach(header => {
 	header.addEventListener('click', e => {
+		// When a header cell is clicked: toggle sorted class so we know which column is being sorted
 		e.target.classList.toggle('sorted');
 		const clickedOn = e.target.dataset.header;
-		console.log(clickedOn);
+
+		// Call sorting function
 		sortMainTableCols(clickedOn, e);
 	});
 });
 
-async function callThings() {
-	await getSummary();
-	test();
-}
-
 window.addEventListener('load', callThings);
-// add chart below clicked table row country
-let country = '';
-function test() {
-	const countryCell = document.querySelectorAll('.main-table-cell:first-child:not(.chart)');
-	console.log(countryCell);
-	countryCell.forEach(el => {
-		el.addEventListener('click', e => {
-			const chart = document.querySelector('.chart');
-			console.dir(chart);
-			console.dir(e.target);
-			if (chart !== null) {
-				chart.remove();
+
+// Function that deals with creating a new chart container element everytime a country cell is clicked
+function createChartContainer() {
+	let country = '';
+	// Select all cells with a country name in it
+	const countryCells = document.querySelectorAll('.main-table-cell:first-child:not(.chart)');
+	countryCells.forEach(cell => {
+		cell.addEventListener('click', e => {
+			// If chart already exists or user clicked the same country, remove the chart
+			// Also remove the calendar that flatpickr appends to the body
+			const chartContainer = document.querySelector('.chart-container');
+			if (chartContainer !== null) {
+				const pickerEl = document.querySelector('.flatpickr-calendar');
+				pickerEl.remove();
+				const containsActiveClass = chartContainer.classList.contains('active');
+				const sameClickedCountry = chartContainer.previousElementSibling === e.target.parentElement;
+				if (containsActiveClass && sameClickedCountry) {
+					chartContainer.remove();
+					return;
+				}
+				chartContainer.remove();
 			}
 
-			country = e.target.innerText.toLowerCase().replace('(', '').replace(')', '');
+			// Create elements for the chart and it's container and insert them after the clicked parents row element
+			const newChartContainer = document.createElement('div');
+			newChartContainer.classList.add('chart-container', 'active');
 			const clickedParent = e.target.parentElement;
-			const newRow = document.createElement('div');
-			newRow.classList.add('chart-container');
-			const newDiv = document.createElement('div');
-			newDiv.classList.add('chart');
-			newRow.appendChild(newDiv);
-			clickedParent.insertAdjacentElement('afterend', newRow);
-			console.log(country);
-			if (country) {
-				getData(country);
-			}
+			clickedParent.insertAdjacentElement('afterend', newChartContainer);
+			const newChart = document.createElement('div');
+			newChart.classList.add('chart');
+			newChartContainer.appendChild(newChart);
+
+			// Get country name and call getData function
+			country = e.target.innerText.toLowerCase().replace('(', '').replace(')', '');
+			getData(country);
 		});
 	});
 }
 
-const picker = document.querySelector('#picker');
-flatpickr(picker, {
-	mode       : 'range',
-	maxDate    : 'today',
-	dateFormat : 'Y-m-d'
-});
-
 async function getData(country) {
-	console.log(country);
-	let arr = [];
+	// Create dates to use in the api call
 	const firstDate = dayjs().subtract(14, 'days').format('YYYY-MM-DD') + 'T00:00:00Z';
 	const today = dayjs().format('YYYY-MM-DD') + 'T00:00:00Z';
 	const res = await fetch(
 		`https://api.covid19api.com/total/country/${country}/status/confirmed?from=${firstDate}&to=${today}`
 	);
-	console.log(`https://api.covid19api.com/total/country/${country}/status/confirmed?from=${firstDate}&to=${today}`);
 	const data = await res.json();
-	console.log(data);
+
+	// Push only the keys and values we want from the api into an array; this is the data the chart will display
+	let chartDataArr = [];
 	data.forEach(element => {
-		arr.push({ x: element.Date.replace('T00:00:00Z', ''), y: element.Cases });
+		chartDataArr.push({ x: element.Date.replace('T00:00:00Z', ''), y: element.Cases });
 	});
+
+	// Use array as an argument when calling renderChart
+	renderChart(chartDataArr);
+}
+
+function datePickerRender(chart) {
+	console.log('DATEPICKERRENDER');
+
+	// Create a new input element for the date picker and render it
+	const picker = document.createElement('input');
+	picker.classList.add('picker');
+	chart.parentElement.prepend(picker);
+	flatpickr(picker, {
+		mode       : 'range',
+		maxDate    : 'today',
+		dateFormat : 'Y-m-d'
+	});
+}
+
+function renderChart(data) {
+	console.log('RENDERCHART');
+
+	// Create chart options
 	let options = {
-		chart  : {
-			height : 400,
-			type   : 'area'
+		chart      : {
+			height     : 400,
+			type       : 'line',
+			dropShadow : {
+				enabled : true,
+				color   : '#000',
+				top     : 18,
+				left    : 7,
+				blur    : 10,
+				opacity : 0.2
+			}
 		},
-		series : [
+		stroke     : {
+			curve : 'smooth'
+		},
+		series     : [
 			{
-				name : 'Cases',
-				data : arr
+				name : 'Total Cases',
+				data : data
 			}
 		],
-		xaxis  : {
+		xaxis      : {
 			type   : 'datetime',
 			labels : {
 				datetimeFormatter : {
 					year  : 'yyyy',
 					month : "MMM 'yy"
 				}
+			},
+			title  : {
+				text : 'Date'
 			}
 		},
-		colors : [ 'rgb(78, 125, 228)' ]
+		yaxis      : {
+			title : {
+				text : 'Total Cases'
+			}
+		},
+		colors     : [ 'rgb(78, 125, 228)' ],
+		dataLabels : {
+			enabled : true
+		}
 	};
 
-	let chart = new ApexCharts(document.querySelector('.chart'), options);
+	// Render the chart inside chartEl
+	const chartEl = document.querySelector('.chart');
+	let chart = new ApexCharts(chartEl, options);
 	chart.render();
+
+	// Call datePickerRender so that a date picker will be prepended to the chartEl
+	datePickerRender(chartEl);
+}
+
+async function callThings() {
+	await getSummary();
+	createChartContainer();
 }
